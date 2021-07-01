a5528fdc9057f51b9f777d89f1d924e0cc7e3b63
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-07-01 00:38:00 -0300

    Design da tela de login

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,16 +6,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/LoginView.fxml"));
+            Parent parent = loader.load();
 
+            Scene cenaPrincipal = new Scene(parent);
+            primaryStage.setScene(cenaPrincipal);
+            primaryStage.setTitle("Tela de Login - Pitang");
+            primaryStage.show();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
